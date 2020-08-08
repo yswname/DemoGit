ffe51e902d93a0399a97c802081cc4e6686541ac
Noble Yang <775488842@qq.com>
2020-08-08 10:46:29 +0800

    初始化项目

---
+++ src/main/java/cn/com/demo/springboot/demogithub01/Demogithub01Application.java
@@ -0,0 +1,13 @@
+package cn.com.demo.springboot.demogithub01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demogithub01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demogithub01Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/demo/springboot/demogithub01/Demogithub01ApplicationTests.java
@@ -0,0 +1,12 @@
+package cn.com.demo.springboot.demogithub01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest(classes = Demogithub01Application.class)
+class Demogithub01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
